9c9267c39e0a5bf2edf29a9205621320eb9c5001
Yurchyk Yurii <124770215+yurchykyur@users.noreply.github.com>
2023-07-21 12:33:27 +0000

    Update App.jsx and PixabayAPI.js (used as function)

--- src/components/Services/PixabayAPI.js
+++ src/components/Services/PixabayAPI.js
@@ -1,56 +1,26 @@
 import axios from 'axios';
 
-export default class servicePixabayAPI {
-  constructor() {
-    this.page = 1;
-    this.searchQuery = '';
-    this.totalHits = 0;
-    this.hitsPerPage = 12;
-  }
+export default async function servicePixabayAPI(
+  searchQuery,
+  page,
+  hitsPerPage
+) {
+  const API_KEY = '36923827-7e58aafe5e36d67095a3a9316';
 
-  async getImages() {
-    const API_KEY = '36923827-7e58aafe5e36d67095a3a9316';
-
-    const config = {
-      method: 'get',
-      baseURL: 'https://pixabay.com/api/',
-      params: {
-        key: API_KEY,
-        q: this.searchQuery,
-        page: this.page,
-        per_page: this.hitsPerPage,
-        image_type: 'photo',
-        orientation: 'horizontal',
-        safesearch: true,
-      },
-    };
-
-    const response = await axios.get('', config);
-    this.increasePage();
-    return response.data;
-  }
-
-  get query() {
-    return this.searchQuery;
-  }
-
-  set query(newQuery) {
-    this.searchQuery = newQuery;
-  }
-
-  get hits() {
-    return this.totalHits;
-  }
-
-  set hits(newTotalHits) {
-    this.totalHits = newTotalHits;
-  }
-
-  increasePage() {
-    this.page += 1;
-  }
-
-  resetPage = () => {
-    this.page = 1;
+  const config = {
+    method: 'get',
+    baseURL: 'https://pixabay.com/api/',
+    params: {
+      key: API_KEY,
+      q: searchQuery,
+      page: page,
+      per_page: hitsPerPage,
+      image_type: 'photo',
+      orientation: 'horizontal',
+      safesearch: true,
+    },
   };
+
+  const response = await axios.get('', config);
+  return response.data;
 }
